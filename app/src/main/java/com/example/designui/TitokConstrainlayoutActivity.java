44f08dc92b56e24148a907a13e2d622a1bae4351
vuthin <thiravuthin.thoeun@cubetiqs.com>
2022-09-10 08:45:48 -0500

    more UI

---
+++ app/src/main/java/com/example/designui/TitokConstrainlayoutActivity.java
@@ -0,0 +1,14 @@
+package com.example.designui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TitokConstrainlayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_titok_constrainlayout);
+    }
+}
